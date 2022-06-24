d1a02ce13e36c51d450e7d4716fc683c0d653b12
rakib-cse-sust <rakib.cse.sust@gmail.com>
2022-06-24 17:27:34 +0100

    FasterPaymentsTests test done

--- ClearBank.DeveloperTest.Tests/FasterPaymentsTests.cs
+++ ClearBank.DeveloperTest.Tests/FasterPaymentsTests.cs
@@ -31,5 +31,72 @@ namespace ClearBank.DeveloperTest.Tests
 
             Assert.True(result.Success);
         }
+
+        [Fact]
+        public void FasterPayments_ReturnPaymentResult_ForAccountIsnull_Invalid()
+        {
+            var request = new MakePaymentRequest()
+            {
+                Amount = 2000,
+                DebtorAccountNumber = "AB1234",
+                PaymentScheme = PaymentScheme.FasterPayments
+            };
+
+            IPaymentSchemeProvider provider = new FasterPayments();
+
+            var result = provider.GetPaymentResult(null, request);
+
+            Assert.True(result.Success);
+        }
+
+        [Fact]
+        public void FasterPayments_ReturnPaymentResult_ForDifferentPaymentSchemes_Invalid()
+        {
+            var request = new MakePaymentRequest()
+            {
+                Amount = 500,
+                DebtorAccountNumber = "AB1234",
+                PaymentScheme = PaymentScheme.FasterPayments
+            };
+
+            var account = new Account()
+            {
+                AccountNumber = "AB1234",
+                Status = AccountStatus.Live,
+                Balance = 1500,
+                AllowedPaymentSchemes = AllowedPaymentSchemes.Bacs
+            };
+
+            IPaymentSchemeProvider provider = new FasterPayments();
+
+            var result = provider.GetPaymentResult(account, request);
+
+            Assert.True(result.Success);
+        }
+
+        [Fact]
+        public void FasterPayments_ReturnPaymentResult_ForOverdraft_Invalid()
+        {
+            var request = new MakePaymentRequest()
+            {
+                Amount = 2000,
+                DebtorAccountNumber = "AB1234",
+                PaymentScheme = PaymentScheme.FasterPayments
+            };
+
+            var account = new Account()
+            {
+                AccountNumber = "AB1234",
+                Status = AccountStatus.Live,
+                Balance = 1500,
+                AllowedPaymentSchemes = AllowedPaymentSchemes.FasterPayments
+            };
+
+            IPaymentSchemeProvider provider = new FasterPayments();
+
+            var result = provider.GetPaymentResult(account, request);
+
+            Assert.True(result.Success);
+        }
     }
 }
